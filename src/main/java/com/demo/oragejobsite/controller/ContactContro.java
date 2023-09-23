0ebb4b8bf4797db674bfea5223ef8671ef74825f
shrey_akan <shereyansjain0904@gmail.com>
2023-09-23 02:11:52 -0700

    front contact

--- src/main/java/com/demo/oragejobsite/controller/ContactContro.java
+++ src/main/java/com/demo/oragejobsite/controller/ContactContro.java
@@ -1,5 +1,26 @@
 package com.demo.oragejobsite.controller;
 
-public class ContactContro {
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
 
+import com.demo.oragejobsite.dao.Contactfrontdao;
+import com.demo.oragejobsite.entity.DirectConntact;
+
+
+
+@CrossOrigin(origins="http://159.203.168.51")
+@RestController
+public class ContactContro {
+	@Autowired
+	private Contactfrontdao cfd;
+	@CrossOrigin(origins="http://159.203.168.51")
+	@PostMapping("/contactform")
+	public DirectConntact contactform(@RequestBody DirectConntact frontcon)
+	{
+			return cfd.save(frontcon);
+		
+	}
 }
